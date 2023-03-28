78c9436bf87fe14ade9725e5c26b909c693bc577
sadat.malik <sadat.malik@me.com>
2023-03-28 21:31:00 +0100

    initial files

---
+++ src/main/java/com/creativefusion/spring6resttemplate/Spring6RestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.creativefusion.spring6resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6RestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6RestTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/creativefusion/spring6resttemplate/Spring6RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creativefusion.spring6resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
