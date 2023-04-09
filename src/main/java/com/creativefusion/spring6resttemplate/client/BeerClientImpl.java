5df1e02d01a30ba62091e8ce3b92a8940e5c1a4c
sadat.malik <sadat.malik@me.com>
2023-04-09 12:42:38 +0300

    set up mock test with mock rest service server

--- src/main/java/com/creativefusion/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/com/creativefusion/spring6resttemplate/client/BeerClientImpl.java
@@ -23,8 +23,8 @@ import java.util.UUID;
 @Service
 public class BeerClientImpl implements BeerClient {
 
-    private static final String GET_BEER_PATH = "/api/v1/beer";
-    private static final String GET_BEER_BY_ID_PATH = "/api/v1/beer/{beerId}";
+    public static final String GET_BEER_PATH = "/api/v1/beer";
+    public static final String GET_BEER_BY_ID_PATH = "/api/v1/beer/{beerId}";
 
     private final RestTemplateBuilder restTemplateBuilder;
 
